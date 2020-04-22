6d2e4eb707adae553f39060e1b3736f30e64edd8
Louis Ritter <louritter1@gmail.com>
2020-04-22 11:09:05 -0400

    fixed test error

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ module MarketPlaceApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.eager_load_paths << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -33,5 +32,7 @@ module MarketPlaceApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.eager_load_paths << Rails.root.join('lib')
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
   #API def
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: %i[show create update destroy]
       resources :tokens, only: [:create]
+
+      resources :users, only: %i[show create update destroy]
     end
   end
 end
