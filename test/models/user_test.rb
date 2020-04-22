049a197eda4aa19bc234ab0e5475a8e4ab200833
Louis Ritter <louritter1@gmail.com>
2020-04-22 13:37:11 -0400

    Generated product model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -16,4 +16,10 @@ class UserTest < ActiveSupport::TestCase
     password_digest: 'test')
     assert user.valid?
   end
+
+  test 'destroy user should destroy linked product' do 
+    assert_difference('Product.count', -1) do
+      users(:one).destroy
+    end
+  end
 end
