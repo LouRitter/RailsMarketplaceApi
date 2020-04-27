814bc94e7684eada1bed6829ebd602b81dc21c73
Louis Ritter <louritter1@gmail.com>
2020-04-26 22:40:13 -0400

    Order placed with product quantity

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -6,7 +6,17 @@ class OrderTest < ActiveSupport::TestCase
     @product1 = products(:one)
     @product2 = products(:two)
   end
+ test 'builds 2 placements for the order' do
+    @order.build_placements_with_product_ids_and_quantities [
+      { product_id: @product1.id, quantity: 2 },
+      { product_id: @product2.id, quantity: 3 },
+    ]
 
+    assert_difference('Placement.count', 2) do
+      @order.save
+    end
+  end
+  
   test "should set total" do
     order = Order.new user_id: @order.user_id
     order.products << products(:one)
